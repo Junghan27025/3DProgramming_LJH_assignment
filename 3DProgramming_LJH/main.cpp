ea1e2449fc30ed48891044a7b202627aeb5ce45c
LJH <lnjmsyohoo@gmail.com>
2022-11-12 14:46:53 +0900

    [3주차]원 만들기

--- 3DProgramming_LJH/main.cpp
+++ 3DProgramming_LJH/main.cpp
@@ -15,7 +15,7 @@ static void key_callback(GLFWwindow* window, int key, int scancode, int action, 
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
-
+     
 }
 
 
